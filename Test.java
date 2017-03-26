b449f8ab6b0669533423c9a7328ce198779bd189
cxxyjsj <809349617@qq.com>
2017-03-26 21:10:07 +0800

    update

--- Test.java
+++ Test.java
@@ -1,11 +1,7 @@
 package com.demo.test;
 
-<<<<<<< HEAD
 // Add comments
-=======
-
 // Del comments
->>>>>>> dev
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Just for test");
